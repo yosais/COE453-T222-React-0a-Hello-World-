365cbb75afcbfd0bda17bb3bc18c69a0bcb6f261
yosais <yosais@kfupm.edu.sa>
2023-04-02 16:44:02 +0300

    1 file modified

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
-function Button(props) {
-  let { action, title } = props;
-  return <button onClick={action}>{title}</button>;
+function HelloComponent() {
+  return <h1>Hello World</h1>;
 }
 
 function App() {
-  const [count, setCount] = React.useState(0);
-
-  let incrementCount = () => {
-    setCount(count + 1);
-  };
-
-  let decrementCount = () => {
-    setCount(count - 1);
-  };
-
-  let resetCount = () => {
-    setCount(0);
-  };
-
-  return (
-    <div className="app">
-              <p>Count: {count}</p>
-              
-      <div className="buttons">
-                  
-        <Button title={'Decrement'} action={decrementCount} />
-                  
-        <Button title={'Increment'} action={incrementCount} />
-                  
-        <Button title={'Reset'} action={resetCount} />
-                
-      </div>
-            
-    </div>
-  );
+  return <HelloComponent />;
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
